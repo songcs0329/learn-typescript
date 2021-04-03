d693bdba8599af63691787185c9ceb5e09caa943
songcs <songcs0329@gmail.com>
2021-04-03 15:29:20 +0900

    210403 first commit + 타입스크립트 소개&배경

---
+++ why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// sum(10, '20') // 1020
+// var result = sum(10, 20);
+// result.toLocaleString();
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a first number
+ * @param {number} b second number
+ */
+function sum(a, b) {
+  return a + b;
+}
+sum(10, '20');
